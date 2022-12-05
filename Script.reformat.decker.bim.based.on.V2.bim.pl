ed47a24a5fddfc1401095caa7c95b28224b3197f
Jinpeng Wang <wangjinpeng0225@163.com>
2022-12-05 14:57:55 +0800

    Update Script.reformat.decker.bim.based.on.V2.bim.pl

--- Script.reformat.decker.bim.based.on.V2.bim.pl
+++ Script.reformat.decker.bim.based.on.V2.bim.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
 use IO::File;
-my $fh_in = IO::File->new("decker.bim",'r');
+my $fh_in = IO::File->new("decker.bim.bak",'r');
 my $fh_out = IO::File->new(">decker.V2.bim");
-my $fh_50K_V2 = IO::File->new("/public/home/wangjinpeng/01.cattle/07.WIDDE_50K_3509Samples/02.50K_V2/50K_V2_filter.bim",'r');
+my $fh_50K_V2 = IO::File->new("/public/home/wangjinpeng/01.cattle/07.WIDDE_and_Decker_50K/02.50K_V2/50K_V2_filter.bim",'r');
 my %V2;
 while(<$fh_50K_V2>){
 	chomp;
@@ -14,17 +14,29 @@ while(<$fh_50K_V2>){
 	$V2{$loci}{allele1}=$allele1;
 	$V2{$loci}{allele2}=$allele2;
 }
+
+my %decker;
+
 while(<$fh_in>){
 	chomp;
 	my $line = $_;
 	my @eles = split /\s+/, $line;
 	my ($chr, $snpid, $zero, $pos, $allele1, $allele2)=@eles;
 	my $loci="$chr-$pos";
+
 	if(exists $V2{$loci}){
 		my $V2_snpid=$V2{$loci}{snpid};
 		my $V2_allele1=$V2{$loci}{allele1};
 		my $V2_allele2=$V2{$loci}{allele2};
 		my $V2_geno="$V2_allele1$V2_allele2";
+		if(exists $decker{$loci} ){
+			$V2_snpid="$V2_snpid-dup";
+		}else{
+			$decker{$loci}=1
+		}
+		
+
+
 		if($V2_geno=~/A/){
 			my $new_decker_B;
 			if($V2_allele1 eq "A"){
